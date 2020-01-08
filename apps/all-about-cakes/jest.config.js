6fc5622a61d18718ca0e347e14b6d7c7bc0ba5e9
IanKlek <ian.klek@gmail.com>
2020-01-08 21:18:33 +0000

    Revert

--- apps/all-about-cakes/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'all-about-cakes',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/all-about-cakes',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
