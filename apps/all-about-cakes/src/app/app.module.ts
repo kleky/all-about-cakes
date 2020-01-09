99820e336e1f84af8f988988483245817afb6176
IanKlek <ian.klek@gmail.com>
2020-01-09 19:27:11 +0000

    Added routing to add cake component

--- apps/all-about-cakes/src/app/app.module.ts
+++ apps/all-about-cakes/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
 import { CakesModule } from './cakes/cakes.module';
 
+const appRoutes: Routes = [
+  { path: '', component: AppComponent },
+];
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
     BrowserModule,
     HttpClientModule,
     CoreModule,
--- apps/all-about-cakes/src/app/cakes/cakes.module.ts
+++ apps/all-about-cakes/src/app/cakes/cakes.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CakesComponent } from './cakes.component';
 import { DataStoreModule } from '../store/data-store.module';
-
-
+import { AddCakeComponent } from './add-cake/add-cake.component';
+import { CakesRoutingModule } from './cakes-routing.module';
 
 @NgModule({
-  declarations: [CakesComponent],
+  declarations: [CakesComponent, AddCakeComponent],
   imports: [
     CommonModule,
     DataStoreModule,
+    CakesRoutingModule,
   ],
+  entryComponents: [ AddCakeComponent ],
   exports: [
     CakesComponent,
+    AddCakeComponent,
   ]
 })
 export class CakesModule { }
