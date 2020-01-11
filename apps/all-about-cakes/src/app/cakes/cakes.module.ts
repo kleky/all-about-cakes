b6037f8fa79d6bce0a707a5ed838b849e9ced0e5
IanKlek <ian.klek@gmail.com>
2020-01-11 08:42:51 +0000

    Tidy up

--- apps/all-about-cakes/src/app/cakes/cakes.module.ts
+++ apps/all-about-cakes/src/app/cakes/cakes.module.ts
@@ -2,17 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CakesComponent } from './cakes.component';
 import { DataStoreModule } from '../store/data-store.module';
-import { AddCakeComponent } from './add-cake/add-cake.component';
-import { CakesRoutingModule } from './cakes-routing.module';
 
 @NgModule({
-  declarations: [CakesComponent, AddCakeComponent],
+  declarations: [CakesComponent],
   imports: [
     CommonModule,
     DataStoreModule,
-    CakesRoutingModule,
   ],
-  entryComponents: [ AddCakeComponent ],
   exports: [
     CakesComponent,
   ]
